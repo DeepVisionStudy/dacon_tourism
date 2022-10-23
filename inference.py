4ff585f47ac5a1a9cb4cffdbf70de0ad30f9fcb4
Dongwoo-Im <iwd960510@gmail.com>
2022-10-23 13:21:42 +0900

    add multi_label & mask_label

--- inference.py
+++ inference.py
@@ -6,22 +6,87 @@ from tqdm import tqdm
 from sklearn.metrics import accuracy_score, f1_score, classification_report
 
 import torch
+import torch.nn as nn
 from transformers import AutoTokenizer, AutoFeatureExtractor
 
 from dataset import create_data_loader_test
-from model import TourClassifier
+from model import TourClassifier, TourClassifier_Continuous
 from utils import set_seeds, load_config
 
 
 PATH_BASE = './'
 PATH_DATA = osp.join(PATH_BASE, 'data')
 
+df = pd.read_csv(osp.join(PATH_DATA, 'train.csv'))
 
-def inference(model, data_loader, device):
+cat1_labels = sorted(list(set(df['cat1'].values.tolist())))
+cat2_labels = sorted(list(set(df['cat2'].values.tolist())))
+cat3_labels = sorted(list(set(df['cat3'].values.tolist())))
+
+cat1_to_cat2_dict = dict()
+for label in cat1_labels:
+    tmp = list(set(df[df['cat1'] == label]['cat2'].values))
+    cat1_to_cat2_dict[cat1_labels.index(label)] = sorted([cat2_labels.index(i) for i in tmp])
+
+cat1_to_cat3_dict = dict()
+for label in cat1_labels:
+    tmp = list(set(df[df['cat1'] == label]['cat3'].values))
+    cat1_to_cat3_dict[cat1_labels.index(label)] = sorted([cat3_labels.index(i) for i in tmp])
+
+cat2_to_cat1_dict = dict()
+for label in cat2_labels:
+    tmp = list(set(df[df['cat2'] == label]['cat1'].values))
+    cat2_to_cat1_dict[cat2_labels.index(label)] = sorted([cat1_labels.index(i) for i in tmp])
+
+cat2_to_cat3_dict = dict()
+for label in cat2_labels:
+    tmp = list(set(df[df['cat2'] == label]['cat3'].values))
+    cat2_to_cat3_dict[cat2_labels.index(label)] = sorted([cat3_labels.index(i) for i in tmp])
+
+cat3_to_cat1_dict = dict()
+for label in cat3_labels:
+    tmp = list(set(df[df['cat3'] == label]['cat1'].values))
+    cat3_to_cat1_dict[cat3_labels.index(label)] = sorted([cat1_labels.index(i) for i in tmp])
+
+cat3_to_cat2_dict = dict()
+for label in cat3_labels:
+    tmp = list(set(df[df['cat3'] == label]['cat2'].values))
+    cat3_to_cat2_dict[cat3_labels.index(label)] = sorted([cat2_labels.index(i) for i in tmp])
+
+
+def consider_multi_label(outputs, outputs2, outputs3, cat, dict1, dict2):
+    softmax = nn.Softmax(dim=1)
+    outputs, outputs2, outputs3 = softmax(outputs), softmax(outputs2), softmax(outputs3)
+    
+    if cat == 'cat1':
+        main, sub1, sub2 = outputs, outputs2, outputs3
+    elif cat == 'cat2':
+        sub1, main, sub2 = outputs, outputs2, outputs3
+    elif cat == 'cat3':
+        sub1, sub2, main = outputs, outputs2, outputs3
+    
+    tmp1 = torch.zeros_like(main)
+    for i in range(len(dict1)):
+        tmp1[:, i] = sub1[:, dict1[i]].sum(dim=1)
+    tmp2 = torch.zeros_like(main)
+    for i in range(len(dict2)):
+        tmp2[:, i] = sub2[:, dict2[i]].sum(dim=1)
+    
+    return torch.stack((main, tmp1, tmp2), dim=-1).sum(dim=-1)
+
+
+def mask_with_before_pred(before_pred, logit, dict):
+    output = torch.full(logit.size(), torch.min(logit))
+    for i in range(len(before_pred)):
+        label = int(before_pred[i])
+        for j in dict[label]:
+            output[i, j] = logit[i, j]
+    return output
+
+
+def inference(model, data_loader, device, multi_label, mask_label):
     model = model.eval()
-    preds_arr = []
-    preds_arr2 = []
-    preds_arr3 = []
+    preds_arr, preds_arr2, preds_arr3 = [], [], []
     for d in tqdm(data_loader):
         with torch.no_grad():
             input_ids = d["input_ids"].to(device)
@@ -29,12 +94,20 @@ def inference(model, data_loader, device):
             pixel_values = d['pixel_values'].to(device)
 
             outputs, outputs2, outputs3 = model(
-                input_ids=input_ids,
-                attention_mask=attention_mask,
-                pixel_values=pixel_values
+                input_ids=input_ids, attention_mask=attention_mask, pixel_values=pixel_values
             )
+            
+            if multi_label:
+                outputs = consider_multi_label(outputs, outputs2, outputs3, 'cat1', cat1_to_cat2_dict, cat1_to_cat3_dict)
+                outputs2 = consider_multi_label(outputs, outputs2, outputs3, 'cat2', cat2_to_cat1_dict, cat2_to_cat3_dict)
+                outputs3 = consider_multi_label(outputs, outputs2, outputs3, 'cat3', cat3_to_cat1_dict, cat3_to_cat2_dict)
+            
             _, preds = torch.max(outputs, dim=1)
+            if mask_label:
+                outputs2 = mask_with_before_pred(preds, outputs2, cat1_to_cat2_dict)
             _, preds2 = torch.max(outputs2, dim=1)
+            if mask_label:
+                outputs3 = mask_with_before_pred(preds2, outputs3, cat2_to_cat3_dict)
             _, preds3 = torch.max(outputs3, dim=1)
 
             preds_arr.append(preds.cpu().numpy()[0])
@@ -47,6 +120,8 @@ def inference(model, data_loader, device):
 def get_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('--mode', type=str, default='test')
+    parser.add_argument('--multi_label', action='store_true')
+    parser.add_argument('--mask_label', action='store_true')
     parser.add_argument('--seed', type=int, default=42)
     parser.add_argument('--exp', type=int, default=0)
     parser.add_argument('--epoch', type=int, default=1)
@@ -72,15 +147,18 @@ def main(args, train_config):
     feature_extractor = AutoFeatureExtractor.from_pretrained(train_config.image_model)
 
     eval_data_loader = create_data_loader_test(df, tokenizer, feature_extractor, train_config.max_len)
-
-    model = TourClassifier(
+    if train_config.continuous:
+        model = TourClassifier_Continuous
+    else:
+        model = TourClassifier
+    model = model(
         n_classes1=6, n_classes2=18, n_classes3=128,
         text_model_name=train_config.text_model, image_model_name=train_config.image_model, device=device,
         dropout=train_config.dropout,
     ).to(device)
     model.load_state_dict(torch.load(args.ckpt_dir))
 
-    preds_arr, preds_arr2, preds_arr3 = inference(model, eval_data_loader, device)
+    preds_arr, preds_arr2, preds_arr3 = inference(model, eval_data_loader, device, args.multi_label, args.mask_label)
     
     if args.mode == 'valid':
         for col, arr in zip(['cat1','cat2','cat3'], [preds_arr, preds_arr2, preds_arr3]):
@@ -89,14 +167,17 @@ def main(args, train_config):
             acc = accuracy_score(gt, pred)
             f1 = f1_score(gt, pred, average='weighted')
             print(f"[{col}] acc: {round(acc,4)}, f1_acc: {round(f1,4)}")
+            # print(df[pred != gt]["id"].to_numpy())
             # print(classification_report(gt, pred))
 
     elif args.mode == 'test':
         sample_submission = pd.read_csv(osp.join(PATH_DATA, 'sample_submission.csv'))
         for i in range(len(preds_arr3)):
             sample_submission.loc[i, 'cat3'] = cat3_labels[preds_arr3[i]]
-        sample_submission.to_csv(
-            osp.join(args.work_dir_exp, f'submit_exp{args.exp}_epoch{args.epoch}.csv'), index=False)
+        save_path = f'submit_exp{args.exp}_epoch{args.epoch}'
+        if args.multi_label: save_path += '_multi'
+        if args.mask_label: save_path += '_mask'
+        sample_submission.to_csv(osp.join(args.work_dir_exp, save_path+'.csv'), index=False)
 
 
 if __name__ == '__main__':
